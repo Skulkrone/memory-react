376a534dbece6a89f39d2cb4442f41fea3ac461b
Skulkrone <40754599+Skulkrone@users.noreply.github.com>
2019-02-12 17:26:26 +0100

    first commit-1202

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import GuessCount from './GuessCount';
+import Card from './Card';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    <div className="memory">
+      <GuessCount guesses={0} />
+      <Card card="😃" feedback="hidden" />
+      <Card card="❤️️" feedback="justMatched" />
+      <Card card="🎩" feedback="justMismatched" />
+      <Card card="😺" feedback="visible" />
+      <Card card="🐶" feedback="hidden" />
+      <Card card="🍺" feedback="justMatched" />
+    </div>
+    )
   }
 }
 
-export default App;
+export default App
